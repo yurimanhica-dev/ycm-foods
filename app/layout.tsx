4c1c803df097564f16303fcff01cac4abd74b06d
YuriManhica <156890528+YuriManhica@users.noreply.github.com>
2025-02-17 11:35:23 +0200

    chore: add shadcUi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
