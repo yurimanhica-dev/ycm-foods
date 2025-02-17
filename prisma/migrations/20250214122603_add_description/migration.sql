222f1dbad74307b8d5a56ee3bcca1ed09d1eabbc
YuriManhica <156890528+YuriManhica@users.noreply.github.com>
2025-02-17 10:39:05 +0200

    fix: init project

--- prisma/migrations/20250214122603_add_description/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "description" TEXT,
-ALTER COLUMN "discountPercentage" DROP NOT NULL,
-ALTER COLUMN "discountPercentage" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "Restaurant" ALTER COLUMN "deliveryTimeMinutes" DROP DEFAULT;
